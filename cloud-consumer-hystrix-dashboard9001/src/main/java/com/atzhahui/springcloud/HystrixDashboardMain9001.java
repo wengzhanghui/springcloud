c37f0c59c411e1cb1714e164f59581849ba40818
zhahui <zhahui@116.com>
2022-06-23 19:05:13 +0800

    fourth commit

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atzhahui/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.atzhahui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author zhahui
+ * @creat -06-21 19:35
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
